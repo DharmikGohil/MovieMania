5b3dd0505a8bcfee4cac7a6ba2fe59f2f81a83ac
Dharmik Gohil <gohildharmik2020@gmail.com>
2024-10-14 09:50:57 +0530

    changes made for deployment

--- functions/api.js
+++ functions/api.js
@@ -1,19 +1,4 @@
-const express = require('express');
 const serverless = require('serverless-http');
-
-const app = express();
-
-// Import your routes
-const indexRouter = require('../routes/index');
-// Import other routes as needed
-
-// Use your routes
-app.use('/', indexRouter);
-// Use other routes as needed
-
-// Handle 404 errors
-app.use((req, res) => {
-  res.status(404).send("Sorry, can't find that!");
-});
+const app = require('../app');
 
 module.exports.handler = serverless(app);
